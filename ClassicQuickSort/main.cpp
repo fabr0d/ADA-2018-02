ffc82aa08ae894ea010e7d13f3953f31e99fe15a
Fabrizio <fabrizio.flores@ucsp.edu.pe>
2018-10-23 23:02:54 -0500

    Code

--- ClassicQuickSort/main.cpp
+++ ClassicQuickSort/main.cpp
@@ -1,5 +1,7 @@
 #include<stdio.h> 
 #include<iostream>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 using namespace std;
 
 void swap(int *a, int *b){
@@ -7,52 +9,7 @@ void swap(int *a, int *b){
 	*a = *b;
 	*b = t;
 }
-
-/* This function takes last element as pivot, places
-   the pivot element at its correct position in sorted
-	array, and places all smaller (smaller than pivot)
-   to left of pivot and all greater elements to right
-   of pivot */
-int partition(int arr[], int low, int high)
-{
-	int pivot = arr[high];    // pivot 
-	int i = (low - 1);  // Index of smaller element 
-
-	for (int j = low; j <= high - 1; j++)
-	{
-		// If current element is smaller than or 
-		// equal to pivot 
-		if (arr[j] <= pivot)
-		{
-			i++;    // increment index of smaller element 
-			swap(&arr[i], &arr[j]);
-		}
-	}
-	swap(&arr[i + 1], &arr[high]);
-	return (i + 1);
-}
-
-/* The main function that implements QuickSort
- arr[] --> Array to be sorted,
-  low  --> Starting index,
-  high  --> Ending index */
-void quickSort(int arr[], int low, int high)
-{
-	if (low < high)
-	{
-		/* pi is partitioning index, arr[p] is now
-		   at right place */
-		int pi = partition(arr, low, high);
-
-		// Separately sort elements before 
-		// partition and after partition 
-		quickSort(arr, low, pi - 1);
-		quickSort(arr, pi + 1, high);
-	}
-}
-
-/* Function to print an array */
-void print(int arr[], int size){
+void print(int arr[], int size) {
 	int i;
 	for (i = 0; i < size; i++) {
 		printf("%d ", arr[i]);
@@ -60,13 +17,45 @@ void print(int arr[], int size){
 	cout << " " << endl;
 }
 
-// Driver program to test above functions 
+int partition(int arr[], int low, int high){
+	int pivot = arr[high]; 
+	int i = (low - 1);  // Posicion del menor elemento
+	for (int j = low; j <= high - 1; j++){
+		// si el elemento actual es menos o igual al pivote
+		if (arr[j] <= pivot){
+			i++;    // incrementa en uno la posicion del menor elemento 
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high){
+	if (low < high){
+		//division del arreglo
+		int pi = partition(arr, low, high);
+		//division de los 2 lados recursivamente 
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
 int main(){
-	int arr[] = { 10, 7, 8, 9, 1, 5 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	quickSort(arr, 0, n - 1);
-	printf("Sorted array: ");
-	print(arr, n);
+	cout << "ingrese el numero de elementos: ";
+	int tam;
+	cin >> tam;
+	int *arr = new int[tam];
+	//int eval[7] = { 5,3,7,6,2,1,4 };
+	srand(time(NULL));
+	for (int i = 0; i < tam; i++) {
+		*(arr + i) = rand() % tam+100;
+		//*(arr + i) = eval[i];
+	}
+	print(arr, tam);
+	quickSort(arr, 0, tam - 1);
+	print(arr, tam);
+	delete[] arr;
 	system("pause");
 	return 0;
 }
